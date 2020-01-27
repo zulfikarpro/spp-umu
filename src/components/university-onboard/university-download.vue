fb87e4d0ffe978d32b2906ff772527ff33d3b872
Achmad Rivaldi <rivaldi@hike.id>
2020-01-27 13:55:14 +0700

    Design University Onboard part 3

--- src/components/university-onboard/university-download.vue
+++ src/components/university-onboard/university-download.vue
@@ -7,21 +7,21 @@
             <div class="offset-md-9 offset-4"></div>
             <div class="col-md-1 col-3"><img src="../../assets/logo_umu_orange.png" class="img-fluid" width="90" height="100" alt="logoumu"></div>
         </div>
-        <div class="col-sm-8 mx-auto text-center mt-7" style="font-size:5vw;text-shadow: -1px 0 black;">Selamat Datang di <span style="color:#FF3D12;">UMU</span></div>
+        <div class="col-sm-8 mx-auto text-center mt-7 titletext" style="text-shadow: -1px 0 black;">Selamat Datang di <span style="color:#FF3D12;">UMU</span></div>
         <div class="col-sm-2 mx-auto text-center mb-5"><div class="mt-3 mx-5 lineText"></div></div>
         <div class="col-sm-12 mx-auto text-center mb-5 subtext"> Nikmati kemudahan pembayaran SPP di Aplikasi UMU.<br/> Kemudahan tak hanya dirasakan oleh Sekolah/Universitas, tetapi juga oleh pelajar/mahasiswa.<br/> Masih belum yakin?? Yuk buktikan dengan daftar Sekolah/Universitas Anda sekarang! </div>
-        <div class="col-sm-4 mx-auto text-center mb-7"><a href="#daftarLink" class="btn btn-lg rounded-pill shadow-lg px-5 py-2" style="background-color:#FF3D12;color:white;font-size:1.8vw;" v-smooth-scroll>Daftar Sekarang</a></div>
+        <div class="col-md-4 col-sm-6 mx-auto text-center mb-7"><a href="#daftarLink" class="btn btn-lg rounded-pill shadow-lg px-5 py-2 subtext" style="background-color:#FF3D12;color:white;" v-smooth-scroll>Daftar Sekarang</a></div>
         <div class="mt-7">&nbsp;</div>
         </div>
         <div class="col-sm-12 mx-auto rounded mb-5 " style="color:black;background-color:#E5E5E5;">
-            <div class="mt-7 col-sm-4 mx-auto text-center font-weight-bold" id="daftarLink" style="font-size:3.4vw">Cara Daftar</div>
+            <div class="mt-7 col-md-4 col-sm-6 mx-auto text-center font-weight-bold titletext2" id="daftarLink" >Cara Daftar</div>
             <div class="col-sm-2 mx-auto text-center mb-7"><div class="mt-3 mx-5 lineText"></div></div>
             <div class="col-sm-12 mx-auto row">
-                <div class="col-sm-6 minortext border roundedcust mb-5 py-5 px-4" style="background-color:white;border-color:#d1d1d1!important">
+                <div class="col-md-6 col-sm-12 minortext border roundedcust mb-5 py-5 px-4" style="background-color:white;border-color:#d1d1d1!important">
                 <div class="mb-5 font-weight-bold">Caranya GAMPANG, cukup unduh dan isi dokumen dibawah ini, kemudian kirim ke : <span style="color:#FF3D12;"><u>hello@umu.co.id</u></span></div>
                 <div class="col-sm-6 mx-auto text-center">
                     <div class="btn-group buttonGroup buttonDefault" role="group" aria-label="Basic example">
-                        <button type="button" class="btn " style="background-color:#FF451D;" ><img src="../../assets/downloadformwhite.png" class="img-fluid" alt="dunlud"></button>
+                        <button type="button" @click="pdfTemp" class="btn " style="background-color:#FF451D;" ><img src="../../assets/downloadformwhite.png" class="img-fluid" alt="dunlud"></button>
                         <button type="button" @click="pdfTemp" class="btn py-2 px-4 minortext" style="background-color:#FF3509;color:white;">Unduh Dokumen</button>
                     </div>
                 </div>
@@ -44,7 +44,7 @@
                     </div> -->
                 </div>
                 </div>
-                <div class="col-sm-6 mt-7 text-center"><img src="../../assets/ilustrasi.png" alt="ilustrasi" class="img-fluid"></div>
+                <div class="col-md-6 col-sm-12 mt-7 mb-7 text-center"><img src="../../assets/ilustrasi.png" alt="ilustrasi" class="img-fluid"></div>
             </div>
         </div>
     </div>
@@ -97,17 +97,49 @@ export default {
     color:white;
     font-family: "Roboto Slab";
 }
-.subtext{
-    font-size:1.8vw;
+
+/* Extra small devices (portrait phones, less than 576px) */
+@media (max-width: 575.98px) {
+      .titletext{font-size: 40px;}
+      .titletext2{font-size:39px;}
+      .subtext{font-size:15px !important; }
+      .minortext {font-size:16px !important;}
+ }
+
+/* Small devices (landscape phones, 576px and up) */
+@media (min-width: 576px) and (max-width: 767.98px) {
+      .titletext{font-size: 45px;}
+      .titletext2{font-size:44px;}
+      .subtext{font-size:20px !important; }
+      .minortext {font-size:19px !important;}
 }
-.minortext {
-    font-size:1.5vw;
+
+/* Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) and (max-width: 991.98px) {
+    .titletext{font-size: 50px;}
+    .titletext2{font-size:49px;}
+    .subtext{font-size:26px !important;}
+    .minortext {font-size:24px !important;}
+ }
+
+/* Large devices (desktops, 992px and up) */
+@media (min-width: 992px) and (max-width: 1199.98px) {
+    .titletext{font-size: 55px;}
+    .titletext2{font-size:54px;}
+    .subtext{font-size:33px !important;}
+    .minortext {font-size:26px !important;}
+ }
+
+/* Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {
+.titletext{font-size:96px;}
+.titletext2{font-size:64px;}
+.subtext{font-size:36px !important;}
+.minortext {font-size:28px !important;}
 }
+
 .lineText {
     border-bottom: 6px solid #FF3D12;
-}
-.buttonGroup > button:first-child{
-    cursor: default;
 }
 .buttonGroup > button {
     border-radius: .75rem;
